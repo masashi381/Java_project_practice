0e4caa2207bd888802ea808ce45e9be2ee77038e
masashi381 <121969638+masashi381@users.noreply.github.com>
2024-11-15 09:49:29 -0800

    create repository

--- server/demo/src/main/java/com/example/demo/repository/ForumRepository.java
+++ server/demo/src/main/java/com/example/demo/repository/ForumRepository.java
@@ -1,5 +1,19 @@
 package com.example.demo.repository;
 
-public interface ForumRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.Forum;
+
+@Repository
+public interface ForumRepository extends JpaRepository<Forum, Integer>{
   
 }
+
+/*
+ JpaRepositoryを継承したインターフェースを作成し、
+ データベースを操作します。
+JpaRepositoryを継承することにより、
+データ操作に関するCRUD(作成・取得・更新・削除)のメソッドを
+自動で生成してくれます。
+ */
