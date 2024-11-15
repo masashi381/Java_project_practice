b90eb96d9248a37d123b965530e224e8fc99c5f1
masashi381 <121969638+masashi381@users.noreply.github.com>
2024-11-15 09:56:50 -0800

    delete unnecessary code

--- server/demo/src/main/java/com/example/demo/DemoApplication.java
+++ server/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,8 +9,4 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	@RequestMapping
-	public String helloWorld() {
-    return "Hello, World!";
-  }
 }
