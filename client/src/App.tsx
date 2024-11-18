b0201cb5cbcc2c6f0247e0bc05d73d2623a6f77c
masashi381 <121969638+masashi381@users.noreply.github.com>
2024-11-18 08:56:35 -0800

    download tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <>Hello world</>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
