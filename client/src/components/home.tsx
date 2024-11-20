6b85f69f897ea335a175a4bbe6bc18e0b65d6b7b
masashi381 <121969638+masashi381@users.noreply.github.com>
2024-11-20 14:23:04 -0800

    show data

--- client/src/components/home.tsx
+++ client/src/components/home.tsx
@@ -4,24 +4,28 @@ import React, { useEffect, useState } from "react";
 type DataProps = {
   title: string;
   body: string;
+  id: number;
 };
 const Home: React.FC = () => {
   const [fetchedData, setFetchedData] = useState<DataProps[]>([]);
-  // const [data, setData] = useState<{ message: String } | null>(null);
   useEffect(() => {
     const getData = async () => {
       const res = await axios.get("http://localhost:8080/api/");
       console.log("res: ", res.data);
-      // setData(res.data);
       setFetchedData(res.data);
     };
     getData();
   }, []);
-  // console.log("data: ", data);
 
   console.log("data: ", fetchedData);
-  return <>test</>;
-  // return <div>{fetchedData ? <h2>title : {fetchedData.title}</h2> : null}</div>;
+  return fetchedData.map((data) => {
+    return (
+      <div key={data.id} className="flex border w-72">
+        <h2 className="mr-5">{data.title}</h2>
+        <p>{data.body}</p>
+      </div>
+    );
+  });
 };
 
 export default Home;
--- server/demo/src/main/java/com/example/demo/controller/ForumController.java
+++ server/demo/src/main/java/com/example/demo/controller/ForumController.java
@@ -2,10 +2,7 @@ package com.example.demo.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,51 +10,73 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Forum;
-import com.example.demo.service.ForumService;
+import com.example.demo.repository.ForumRepository;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.ui.Model;
+// import com.example.demo.service.ForumService;
 
 @RestController
 @RequestMapping("/api")
 
 public class ForumController {
-  // 特定のアノテーションを付与したクラスのインスタンスを使用可能にする（依存性注入・DI）
-  @Autowired
-  ForumService service;
-  
-  // トップページ（フォームがある画面）
-  // HTTPリクエストの”GET”を受け付けるメソッドに付与するアノテーション
-  @GetMapping("/")
-  public ResponseEntity <List<Forum>> getAllForum() {
-    return ResponseEntity.ok(service.getAll);
+  private final ForumRepository forumRepository;
+
+  public ForumController(ForumRepository forumRepository) {
+    this.forumRepository = forumRepository;
   }
-  // public String top(Model model){
-  //   model.addAttribute("forum", new Forum());
-  //   return "top";
-  // }
 
-  // データベースへの登録
-  // HTTPリクエストの”POST”を受け付けるメソッドに付与するアノテーション
-  // @PostMapping("/create")
-  // public String saveForum(@ModelAttribute Forum forum, Model model){
-  //   // serviceクラスのinsertメソッドを呼び出し、DBに入力値を登録
-  //   service.insert(forum);
-  //   return "result";
-  // }
+  @GetMapping("/")
+  public List<Forum> getAllForum() {
+    return forumRepository.findAll();
+  }
 
-  @GetMapping("/hello")
-    public Message getMessage(){
-      return new Message("Hello, World!");
-    }
-  public static class Message{
-    private String message;
-    public Message(String message) {
-      this.message = message;
-    }
-    public String getMessage() {
-      return message;
-    }
-    public void setMessage(String message) {
-      this.message = message;
-    }
+  @PostMapping("/create")
+  public Forum createForum(@ModelAttribute Forum forum) {
+    return forumRepository.save(forum);
   }
 }
+// public class ForumController {
+//   // 特定のアノテーションを付与したクラスのインスタンスを使用可能にする（依存性注入・DI）
+//   @Autowired
+//   ForumService service;
+  
+//   // トップページ（フォームがある画面）
+//   // HTTPリクエストの”GET”を受け付けるメソッドに付与するアノテーション
+//   @GetMapping("/")
+//   public ResponseEntity <List<Forum>> getAllForum() {
+//     return 
+//   }
+//   // public String top(Model model){
+//   //   model.addAttribute("forum", new Forum());
+//   //   return "top";
+//   // }
+
+//   // データベースへの登録
+//   // HTTPリクエストの”POST”を受け付けるメソッドに付与するアノテーション
+//   // @PostMapping("/create")
+//   // public String saveForum(@ModelAttribute Forum forum, Model model){
+//   //   // serviceクラスのinsertメソッドを呼び出し、DBに入力値を登録
+//   //   service.insert(forum);
+//   //   return "result";
+//   // }
+
+//   @GetMapping("/hello")
+//     public Message getMessage(){
+//       return new Message("Hello, World!");
+//     }
+//   public static class Message{
+//     private String message;
+//     public Message(String message) {
+//       this.message = message;
+//     }
+//     public String getMessage() {
+//       return message;
+//     }
+//     public void setMessage(String message) {
+//       this.message = message;
+//     }
+//   }
+// }
 
